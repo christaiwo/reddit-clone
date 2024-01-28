4e1be4a8e4ec571ab0c3ce30dc275bc1b2aa9412
Taiwo Ijagbemi <christopherijagbemi@gmail.com>
2024-01-28 14:28:25 +0100

    update home screen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -9,6 +9,31 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: IconButton(
+              iconSize: 4.0,
+              onPressed: () {},
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  user.profilePic,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
       body: Center(child: Text(user.name)),
     );
   }
